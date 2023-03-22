7d22fa00b939e980efb5960e2831c999a74c43d5
QuanNA94 <gideonnguyen94@gmail.com>
2023-03-22 16:02:25 +0700

    Anisotropic_Filtering

--- src/client/client.ts
+++ src/client/client.ts
@@ -3,6 +3,12 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'dat.gui'
 
+/** ==============================================================
+ * 
+ *  Anisotropic Filtering allows us to improve the quality of the MIP maps.
+ * 
+  ============================================================== */
+
 /** [1] Scene (Cảnh): là một đối tượng Three.js chứa tất cả các đối tượng,
  * ánh sáng và hiệu ứng cần được vẽ trên màn hình.
  */
@@ -30,7 +36,8 @@ scene2.add(axesHelper2)
  * CubeCamera,... cho phép bạn tạo ra các hiệu ứng khác nhau và điều chỉnh khoảng cách đến các đối tượng trên màn hình.
  */
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 1
+// camera.position.z = 1
+camera.position.set(0, -0.35, 0.2)
 
 /** [3]Renderer (Trình kết xuất): là một đối tượng Three.js để kết xuất các đối tượng trên màn hình.
  *  Trình kết xuất sẽ sử dụng WebGL hoặc các công nghệ tương tự để tạo ra các hình ảnh 3D.
@@ -55,48 +62,48 @@ new OrbitControls(camera, renderer.domElement)
  */
 
 // const planeGeometry = new THREE.PlaneGeometry(3.6, 1.8)
-const planeGeometry1 = new THREE.PlaneGeometry()
-const planeGeometry2 = new THREE.PlaneGeometry()
+const planeGeometry1 = new THREE.PlaneGeometry() //2, 25)
+const planeGeometry2 = new THREE.PlaneGeometry() //2, 25)
 
 // =================================================================================
 
-// const texture1 = new THREE.TextureLoader().load('img/grid.png')
-// const texture2 = texture1.clone()
-
-const mipmap = (size: number, color: string) => {
-    const imageCanvas = document.createElement('canvas') as HTMLCanvasElement
-    const context = imageCanvas.getContext('2d') as CanvasRenderingContext2D
-
-    imageCanvas.width = size
-    imageCanvas.height = size
-    context.fillStyle = '#888888'
-    context.fillRect(0, 0, size, size)
-    context.fillStyle = color
-    context.fillRect(0, 0, size / 2, size / 2)
-    context.fillRect(size / 2, size / 2, size / 2, size / 2)
-    return imageCanvas
-}
-
-const blankCanvas = document.createElement('canvas') as HTMLCanvasElement
-blankCanvas.width = 128
-blankCanvas.height = 128
-
-const texture1 = new THREE.CanvasTexture(blankCanvas)
-texture1.mipmaps[0] = mipmap(128, '#ff0000')
-texture1.mipmaps[1] = mipmap(64, '#00ff00')
-texture1.mipmaps[2] = mipmap(32, '#0000ff')
-texture1.mipmaps[3] = mipmap(16, '#880000')
-texture1.mipmaps[4] = mipmap(8, '#008800')
-texture1.mipmaps[5] = mipmap(4, '#000088')
-texture1.mipmaps[6] = mipmap(2, '#008888')
-texture1.mipmaps[7] = mipmap(1, '#880088')
-texture1.repeat.set(5, 5)
-texture1.wrapS = THREE.RepeatWrapping
-texture1.wrapT = THREE.RepeatWrapping
-
+const texture1 = new THREE.TextureLoader().load('img/grid.png')
 const texture2 = texture1.clone()
-texture2.minFilter = THREE.NearestFilter
-texture2.magFilter = THREE.NearestFilter
+
+// const mipmap = (size: number, color: string): HTMLCanvasElement => {
+//     const imageCanvas = document.createElement('canvas') as HTMLCanvasElement
+//     const context = imageCanvas.getContext('2d') as CanvasRenderingContext2D
+
+//     imageCanvas.width = size
+//     imageCanvas.height = size
+//     context.fillStyle = '#888888'
+//     context.fillRect(0, 0, size, size)
+//     context.fillStyle = color
+//     context.fillRect(0, 0, size / 2, size / 2)
+//     context.fillRect(size / 2, size / 2, size / 2, size / 2)
+//     return imageCanvas
+// }
+
+// const blankCanvas = document.createElement('canvas') as HTMLCanvasElement
+// blankCanvas.width = 128
+// blankCanvas.height = 128
+
+// const texture1 = new THREE.CanvasTexture(blankCanvas)
+// texture1.mipmaps[0] = mipmap(128, '#ff0000')
+// texture1.mipmaps[1] = mipmap(64, '#00ff00')
+// texture1.mipmaps[2] = mipmap(32, '#0000ff')
+// texture1.mipmaps[3] = mipmap(16, '#880000')
+// texture1.mipmaps[4] = mipmap(8, '#008800')
+// texture1.mipmaps[5] = mipmap(4, '#000088')
+// texture1.mipmaps[6] = mipmap(2, '#008888')
+// texture1.mipmaps[7] = mipmap(1, '#880088')
+// texture1.repeat.set(5, 50)
+// texture1.wrapS = THREE.RepeatWrapping
+// texture1.wrapT = THREE.RepeatWrapping
+
+// const texture2 = texture1.clone()
+// texture2.minFilter = THREE.NearestFilter
+// texture2.magFilter = THREE.NearestFilter
 
 /** [5] Material (Vật liệu): là một đối tượng Three.js để đại diện cho màu sắc, độ bóng và ánh sáng của một đối tượng.
  *  Vật liệu có thể được áp dụng cho hình học để tạo ra các hiệu ứng khác nhau,
@@ -148,8 +155,15 @@ const gui = new GUI()
 const textureFolder = gui.addFolder('THREE.Texture')
 textureFolder.add(texture2, 'minFilter', options.minFilters).onChange(() => updateMinFilter())
 textureFolder.add(texture2, 'magFilter', options.magFilters).onChange(() => updateMagFilter())
+textureFolder
+    .add(texture2, 'anisotropy', 1, renderer.capabilities.getMaxAnisotropy())
+    .onChange(() => updateAnistropy())
+
 textureFolder.open()
 
+function updateAnistropy() {
+    material2.map = texture2.clone()
+}
 function updateMinFilter() {
     texture2.minFilter = Number(texture2.minFilter)
     texture2.needsUpdate = true
