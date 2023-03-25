5642ec86ad395e7a35ab688caf222195ad0b6fd2
QuanNA94 <gideonnguyen94@gmail.com>
2023-03-26 00:21:48 +0700

    Directional_Light_Shadow

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,7 +4,8 @@ import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'dat.gui'
 
 /** ==============================================================
- * The Spot Light Shadow uses a PerspectiveCamera frustum to calculate the shadows.
+ * The Directional Light Shadow uses an OrthographicCamera to calculate the shadows, rather than a PerspectiveCamera. 
+ * This is because the light rays from the DirectionalLight are parallel.
   ============================================================== */
 
 /** [1] Scene (Cảnh): là một đối tượng Three.js chứa tất cả các đối tượng,
@@ -17,11 +18,11 @@ scene.add(new THREE.AxesHelper(5))
 /** [7] Light (Ánh sáng): Được sử dụng để tạo ra ánh sáng trong cảnh, giúp các đối tượng 3D có thể được hiển thị rõ ràng hơn.
  *  Three.js hỗ trợ nhiều loại ánh sáng khác nhau, bao gồm AmbientLight, DirectionalLight, và PointLight.
  */
-const light = new THREE.SpotLight()
-light.castShadow = true;
-light.shadow.mapSize.width = 512;
-light.shadow.mapSize.height = 512;
-light.shadow.camera.near = 0.5;
+const light = new THREE.DirectionalLight()
+light.castShadow = true
+light.shadow.mapSize.width = 512
+light.shadow.mapSize.height = 512
+light.shadow.camera.near = 0.5
 light.shadow.camera.far = 100
 scene.add(light)
 
@@ -30,7 +31,8 @@ scene.add(light)
  */
 
 // const helper = new THREE.SpotLightHelper(light)
-const helper = new THREE.CameraHelper(light.shadow.camera);
+//const helper = new THREE.DirectionalLightHelper(light);
+const helper = new THREE.CameraHelper(light.shadow.camera)
 scene.add(helper)
 
 /** [2] Camera (Máy ảnh): là một đối tượng Three.js để đại diện cho góc nhìn của người dùng.
@@ -70,14 +72,14 @@ new OrbitControls(camera, renderer.domElement)
  */
 
 // 1. khởi tạo một đối tượng PlaneGeometry với kích thước 100 đơn vị chiều dài và 20 đơn vị chiều rộng.
-const planeGeometry : THREE.PlaneGeometry = new THREE.PlaneGeometry(100, 20)
+const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(100, 20)
 // 2.  MeshPhongMaterial để tạo chất liệu cho mặt phẳng.
 const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
 // 3. xoay mặt phẳng quanh trục X một góc -90 độ, tức là xoay mặt phẳng ngang xuống.
 plane.rotateX(-Math.PI / 2)
 // 4. đặt vị trí của mặt phẳng tại độ cao -1.75 đơn vị so với trục Y. Điều này đặt mặt phẳng trên mặt đất.
 plane.position.y = -1.75
-plane.receiveShadow = true;
+plane.receiveShadow = true
 
 // 5.  thêm mặt phẳng vào đối tượng Scene để nó được hiển thị lên màn hình.
 scene.add(plane)
@@ -174,20 +176,35 @@ lightFolder.addColor(data, 'color').onChange(() => {
 lightFolder.add(light, 'intensity', 0, 1, 0.01)
 // lightFolder.open()
 
-const spotLightFolder = gui.addFolder('THREE.SpotLight')
-spotLightFolder.add(light, 'distance', 0, 100, 0.01)
-spotLightFolder.add(light, 'decay', 0, 4, 0.1)
-spotLightFolder.add(light, 'angle', 0, 1, 0.1)
-spotLightFolder.add(light, 'penumbra', 0, 1, 0.1)
-spotLightFolder.add(light.shadow.camera, "near", 0.1, 100).onChange(() => light.shadow.camera.updateProjectionMatrix())
-spotLightFolder.add(light.shadow.camera, "far", 0.1, 100).onChange(() => light.shadow.camera.updateProjectionMatrix())
-spotLightFolder.add(data, "shadowMapSizeWidth", [256, 512, 1024, 2048, 4096]).onChange(() => updateShadowMapSize())
-spotLightFolder.add(data, "shadowMapSizeHeight", [256, 512, 1024, 2048, 4096]).onChange(() => updateShadowMapSize())
-
-spotLightFolder.add(light.position, 'x', -50, 50, 0.01)
-spotLightFolder.add(light.position, 'y', -50, 50, 0.01)
-spotLightFolder.add(light.position, 'z', -50, 50, 0.01)
-spotLightFolder.open()
+const directionalLightFolder = gui.addFolder('THREE.DirectionalLight')
+directionalLightFolder
+    .add(light.shadow.camera, 'left', -10, -1, 0.1)
+    .onChange(() => light.shadow.camera.updateProjectionMatrix())
+directionalLightFolder
+    .add(light.shadow.camera, 'right', 1, 10, 0.1)
+    .onChange(() => light.shadow.camera.updateProjectionMatrix())
+directionalLightFolder
+    .add(light.shadow.camera, 'top', 1, 10, 0.1)
+    .onChange(() => light.shadow.camera.updateProjectionMatrix())
+directionalLightFolder
+    .add(light.shadow.camera, 'bottom', -10, -1, 0.1)
+    .onChange(() => light.shadow.camera.updateProjectionMatrix())
+directionalLightFolder
+    .add(light.shadow.camera, 'near', 0.1, 100)
+    .onChange(() => light.shadow.camera.updateProjectionMatrix())
+directionalLightFolder
+    .add(light.shadow.camera, 'far', 0.1, 100)
+    .onChange(() => light.shadow.camera.updateProjectionMatrix())
+directionalLightFolder
+    .add(data, 'shadowMapSizeWidth', [256, 512, 1024, 2048, 4096])
+    .onChange(() => updateShadowMapSize())
+directionalLightFolder
+    .add(data, 'shadowMapSizeHeight', [256, 512, 1024, 2048, 4096])
+    .onChange(() => updateShadowMapSize())
+directionalLightFolder.add(light.position, 'x', -50, 50, 0.01)
+directionalLightFolder.add(light.position, 'y', -50, 50, 0.01)
+directionalLightFolder.add(light.position, 'z', -50, 50, 0.01)
+directionalLightFolder.open()
 
 function updateShadowMapSize() {
     light.shadow.mapSize.width = data.shadowMapSizeWidth
