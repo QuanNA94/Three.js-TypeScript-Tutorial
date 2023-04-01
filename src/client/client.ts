479d8a3d57d8c8d23c68705da0f5f63c8f55dc07
QuanNA94 <gideonnguyen94@gmail.com>
2023-04-01 15:42:46 +0700

    FBX_Animations

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,123 +2,118 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
-/** ==============================================================
- * The FBX format is used to provide interoperability 
- * between digital content creation applications and game engines 
- * such as Blender, Maya, Autodesk, Unity, Unreal and many others. 
- * It supports many features such as 3D models, scene hierarchy, materials, lighting, animations, bones and more.
- * 
- * For this exercise, you should download the character model named "Kachujin G Rosales"
- *  from Mixamo and save it into your ./dist/client/models folder. 
- * Save it using the FBX Binary(.fbx) and T-Pose options.
- * 
- * ----------------NOTE----------------
- * There seems to be a problem with FBXLoader in Three r151. 
- * You may need to downgrade to Three r150.1 to do the FBXLoader lessons.
-
-    npm install three@0.150.1
-  ============================================================== */
-
-/** [1] Scene (Cảnh): là một đối tượng Three.js chứa tất cả các đối tượng,
- * ánh sáng và hiệu ứng cần được vẽ trên màn hình.
- */
-// tạo một đối tượng scene mới,sau đó thêm đối tượng AxesHelper vào scene
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-/** [7] Light (Ánh sáng): Được sử dụng để tạo ra ánh sáng trong cảnh, giúp các đối tượng 3D có thể được hiển thị rõ ràng hơn.
- *  Three.js hỗ trợ nhiều loại ánh sáng khác nhau, bao gồm AmbientLight, DirectionalLight, và PointLight.
- */
 const light = new THREE.PointLight()
-light.position.set(0.8, 1.4, 1.0)
+light.position.set(2.5, 7.5, 15)
 scene.add(light)
 
-const ambientLight = new THREE.AmbientLight()
-scene.add(ambientLight)
+// const ambientLight = new THREE.AmbientLight()
+// scene.add(ambientLight)
 
-/**  AxesHelper là một class của Three.js: tạo 1 trục tọa độ 3D
- *  với các đường dẫn khác màu sắc, ở đây trục có độ dài 5 đơn vị
- */
-
-// const helper = new THREE.SpotLightHelper(light)
-//const helper = new THREE.DirectionalLightHelper(light);
-// const helper = new THREE.CameraHelper(light.shadow.camera)
-// scene.add(helper)
-
-/** [2] Camera (Máy ảnh): là một đối tượng Three.js để đại diện cho góc nhìn của người dùng.
- * Có nhiều loại camera khác nhau như PerspectiveCamera, OrthographicCamera,
- * CubeCamera,... cho phép bạn tạo ra các hiệu ứng khác nhau và điều chỉnh khoảng cách đến các đối tượng trên màn hình.
- */
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-// camera.position.x = 2
-// camera.position.y = 1
-// camera.position.z = 2
-
 camera.position.set(0.8, 1.4, 1.0)
 
-/** [3]Renderer (Trình kết xuất): là một đối tượng Three.js để kết xuất các đối tượng trên màn hình.
- *  Trình kết xuất sẽ sử dụng WebGL hoặc các công nghệ tương tự để tạo ra các hình ảnh 3D.
- */
-const renderer: any = new THREE.WebGLRenderer()
-//renderer.physicallyCorrectLights = true //deprecated
-// renderer.useLegacyLights = false //use this instead of setting physicallyCorrectLights=true property
-// renderer.shadowMap.enabled = true
-// renderer.outputEncoding = THREE.sRGBEncoding
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-
-/** Thêm đối tượng renderer vào thẻ HTML sử dụng hàm appendChild(renderer.domElement).
- *  Trong trường hợp này, renderer.domElement là một đối tượng HTMLCanvasElement,
- *  và nó sẽ được thêm vào thẻ body để hiển thị kết quả cuối cùng của các hoạt động đồ họa.
- */
 document.body.appendChild(renderer.domElement)
-//const material: THREE.MeshPhongMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000, transparent: true })
-//const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
-//scene.add(cube)
-
-/** [4] Geometry (Hình học): là một đối tượng Three.js để đại diện cho hình dạng và kích thước của một đối tượng.
- *  Geometry có thể được sử dụng để tạo ra các hình dạng phức tạp
- * từ các hình dạng cơ bản như hình cầu, hình trụ, hình chữ nhật,...
- */
-
-/** `new OrbitControls(camera, renderer.domElement)`
- * sử dụng trong Three.js để tạo ra một đối tượng điều khiển camera bằng chuột.
- * Nó cung cấp cho người dùng khả năng quay và di chuyển camera trong không gian 3D.
- */
 
 const controls = new OrbitControls(camera, renderer.domElement)
-// cho phép các hiệu ứng nhấp nháy và giảm tốc khi di chuyển camera, giúp tạo ra một trải nghiệm mượt mà hơn khi tương tác với các phần tử 3D.
 controls.enableDamping = true
 controls.target.set(0, 1, 0)
 
 //const material = new THREE.MeshNormalMaterial()
 
-// Note that since Three release 148, you will find the Draco libraries in the `.\node_modules\three\examples\jsm\libs\draco\` folder.
+let mixer: THREE.AnimationMixer
+let modelReady = false
+const animationActions: THREE.AnimationAction[] = []
+let activeAction: THREE.AnimationAction
+let lastAction: THREE.AnimationAction
 
-const fbxLoader = new FBXLoader()
+const fbxLoader: FBXLoader = new FBXLoader()
 fbxLoader.load(
-    // 1. the file to download
-    'models/Kachujin G Rosales.fbx',
-    // 2. what to do on success
+    'models/vanguard_t_choonyung.fbx',
     (object) => {
-        object.traverse(function (child) {
-            console.log( child.name) // whether it's a mesh, light/ camera 
-            // if obj is a Mesh, if it's material => transparent = false
-            if ((child as THREE.Mesh).isMesh) {
-                // (child as THREE.Mesh).material = material
-                if ((child as THREE.Mesh).material) {
-                    ;((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
-                }
-            }
-        })
+        // 1. scale kích thước đối tượng
         object.scale.set(0.01, 0.01, 0.01)
+        mixer = new THREE.AnimationMixer(object)
+
+        /* 2. đoạn code lấy ra hành động animation đầu tiên của đối tượng 
+         và đưa vào trong một mảng animationActions. */
+        const animationAction = mixer.clipAction((object as any).animations[0])
+        // 3. đưa animationAction vào mảng animationActions đã khai báo ở trên
+        animationActions.push(animationAction)
+        animationsFolder.add(animations, 'default')
+        activeAction = animationActions[0]
+
         scene.add(object)
+
+        // add an animation from another file
+        fbxLoader.load(
+            'models/vanguard@samba.fbx',
+            (object) => {
+                console.log('loaded samba')
+
+                const animationAction = mixer.clipAction((object as THREE.Object3D).animations[0])
+                animationActions.push(animationAction)
+                animationsFolder.add(animations, 'samba')
+
+                // add an animation from another file
+                fbxLoader.load(
+                    'models/vanguard@bellydance.fbx',
+                    (object) => {
+                        console.log('loaded bellydance')
+                        const animationAction = mixer.clipAction(
+                            (object as THREE.Object3D).animations[0]
+                        )
+                        animationActions.push(animationAction)
+                        animationsFolder.add(animations, 'bellydance')
+
+                        // add an animation from another file
+                        fbxLoader.load(
+                            'models/vanguard@goofyrunning.fbx',
+                            (object) => {
+                                console.log('loaded goofyrunning')
+                                ;(object as THREE.Object3D).animations[0].tracks.shift() //delete the specific track that moves the object forward while running
+                                // console.dir((object as THREE.Object3D).animations[0])
+                                const animationAction = mixer.clipAction(
+                                    (object as THREE.Object3D).animations[0]
+                                )
+                                animationActions.push(animationAction)
+                                animationsFolder.add(animations, 'goofyrunning')
+
+                                modelReady = true
+                            },
+                            (xhr) => {
+                                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                            },
+                            (error) => {
+                                console.log(error)
+                            }
+                        )
+                    },
+                    (xhr) => {
+                        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+                    },
+                    (error) => {
+                        console.log(error)
+                    }
+                )
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+            },
+            (error) => {
+                console.log(error)
+            }
+        )
     },
-    // progress callback
     (xhr) => {
         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
     },
-    // error callback
     (error) => {
         console.log(error)
     }
@@ -126,43 +121,59 @@ fbxLoader.load(
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    // 1. Cập nhật tỷ lệ khung hình (aspect) của camera theo kích thước mới của window:
     camera.aspect = window.innerWidth / window.innerHeight
-    // 2. Cập nhật ma trận chiếu (projection matrix) của camera:
     camera.updateProjectionMatrix()
-    // 3. Cập nhật kích thước của renderer để phù hợp với kích thước mới của window:
     renderer.setSize(window.innerWidth, window.innerHeight)
-    // 4. Gọi hàm render() để render lại cảnh.
     render()
-    /** Những bước này giúp đảm bảo rằng cảnh được hiển thị đúng tỷ lệ khung hình
-     * và độ phân giải trên màn hình khi kích thước của cửa sổ trình duyệt thay đổi.
-     */
 }
+
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
-// const data = {
-//     color: light.color.getHex(),
-//     // groundColor: light.groundColor.getHex(),
-//     mapsEnabled: true,
-//     shadowMapSizeWidth: 512,
-//     shadowMapSizeHeight: 512,
-// }
+const animations = {
+    default: function () {
+        setAction(animationActions[0])
+    },
+    samba: function () {
+        setAction(animationActions[1])
+    },
+    bellydance: function () {
+        setAction(animationActions[2])
+    },
+    goofyrunning: function () {
+        setAction(animationActions[3])
+    },
+}
 
-// Một hàm animate để cập nhật trạng thái của các đối tượng 3D trong mỗi khung hình (frame)
+const setAction = (toAction: THREE.AnimationAction) => {
+    if (toAction != activeAction) {
+        lastAction = activeAction
+        activeAction = toAction
+        // lastAction.stop()
+        lastAction.fadeOut(1)
+        activeAction.reset()
+        activeAction.fadeIn(1)
+        activeAction.play()
+    }
+}
+
+/** GUI này cho phép người dùng tương tác với các biến
+ * và giá trị trong chương trình thông qua các control được hiển thị trên giao diện.
+ *
+ */
+const gui = new GUI()
+const animationsFolder = gui.addFolder('Animations')
+// đoạn code mở folder đó ra để người dùng có thể nhìn thấy các control bên trong
+animationsFolder.open()
+
+const clock = new THREE.Clock()
+
 function animate() {
     requestAnimationFrame(animate)
 
     controls.update()
 
-    // helper.update()
-
-    // torus.forEach((t) => {
-    //     t.rotation.y += 0.01
-    // })
-
-    // trackball controls needs to be updated in the animation loop before it will work
-    // controls.update()
+    if (modelReady) mixer.update(clock.getDelta())
 
     render()
 
@@ -172,4 +183,5 @@ function animate() {
 function render() {
     renderer.render(scene, camera)
 }
+
 animate()
