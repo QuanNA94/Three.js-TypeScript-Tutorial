2f7512776caf08e431c4f0232137db10cfc48973
QuanNA94 <gideonnguyen94@gmail.com>
2023-04-05 22:25:18 +0700

    Raycaster_Mouse_Picking

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,24 +1,20 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 /** ==============================================================
- * While raycasting is almost always used for mouse picking objects in the 3D scene, 
- * it can also be used for simple collision detection.
+ * This is a basic example of using the Raycaster to mouse pick objects in the scene.
+ * The scene is traversed and all individual objects are added to the pickableObjects array that is used by the Raycaster. 
+ * The sphere and plane are deliberately excluded from this so they will not be mouse picked.
  * 
- * In this example, I detect whether the orbit controls will penetrate another object 
- * and adjust the cameras position so that it stays outside.
- * 
- * Essentially, I am creating a ray from the camera target to the camera position. 
- * If there is an intersected object between, then the camera position is adjusted to the intersect point. 
- * This prevents the camera from going behind a wall, or inside a box, or floor, 
- * or any object which is part of the objects array being tested for an intersect.
- * 
- * Also, instead of using the raycaster to find the new point to position the camera in case of collision between the target and itself, 
- * I could instead modify the opacity of the object in between and not move the camera. 
- * Rotate the camera and notice how any object between the camera target and the camera itself, becomes transparent.
+ * The plane also receives shadows while everything else only casts shadows.
 
- * 
+ * This example demonstrates,
+
+    Loading a GLB scene, traversing the child objects to individually add and/or copy properties for later use.
+    Using the Raycaster to detect if the mouse is over certain objects and changing there material.
+
  * ----------------NOTE----------------
  * There seems to be a problem with FBXLoader in Three r151. 
  * You may need to downgrade to Three r150.1 to do the FBXLoader lessons.
@@ -35,13 +31,12 @@ scene.add(new THREE.AxesHelper(5))
 /** [7] Light (Ánh sáng): Được sử dụng để tạo ra ánh sáng trong cảnh, giúp các đối tượng 3D có thể được hiển thị rõ ràng hơn.
  *  Three.js hỗ trợ nhiều loại ánh sáng khác nhau, bao gồm AmbientLight, DirectionalLight, và PointLight.
  */
-// const light1 = new THREE.PointLight(0xffffff, 2)
-// light1.position.set(2.5, 2.5, 2.5)
-// scene.add(light1)
-
-// const light2 = new THREE.PointLight(0xffffff, 2)
-// light2.position.set(-2.5, 2.5, 2.5)
-// scene.add(light2)
+const light = new THREE.SpotLight()
+light.position.set(12.5, 12.5, 12.5)
+light.castShadow = true
+light.shadow.mapSize.width = 1024
+light.shadow.mapSize.height = 1024
+scene.add(light)
 
 /**  AxesHelper là một class của Three.js: tạo 1 trục tọa độ 3D
  *  với các đường dẫn khác màu sắc, ở đây trục có độ dài 5 đơn vị
@@ -59,9 +54,9 @@ scene.add(new THREE.AxesHelper(5))
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 // camera.position.x = 2
 // camera.position.y = 1
-camera.position.z = 2
+// camera.position.z = 2
 
-// camera.position.set(0.8, 1.4, 1.0)
+camera.position.set(15, 15, 15)
 
 /** [3]Renderer (Trình kết xuất): là một đối tượng Three.js để kết xuất các đối tượng trên màn hình.
  *  Trình kết xuất sẽ sử dụng WebGL hoặc các công nghệ tương tự để tạo ra các hình ảnh 3D.
@@ -69,8 +64,8 @@ camera.position.z = 2
 const renderer: any = new THREE.WebGLRenderer()
 //renderer.physicallyCorrectLights = true //deprecated
 // renderer.useLegacyLights = false //use this instead of setting physicallyCorrectLights=true property
-// renderer.shadowMap.enabled = true
-// renderer.outputEncoding = THREE.sRGBEncoding
+renderer.shadowMap.enabled = true
+renderer.outputEncoding = THREE.sRGBEncoding
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 /** Thêm đối tượng renderer vào thẻ HTML sử dụng hàm appendChild(renderer.domElement).
@@ -92,89 +87,50 @@ document.body.appendChild(renderer.domElement)
  * Nó cung cấp cho người dùng khả năng quay và di chuyển camera trong không gian 3D.
  */
 
-const raycaster = new THREE.Raycaster()
-const sceneMeshes: THREE.Mesh[] = []
-const dir = new THREE.Vector3()
-let intersects: THREE.Intersection[] = []
-
 const controls = new OrbitControls(camera, renderer.domElement)
 // cho phép các hiệu ứng nhấp nháy và giảm tốc khi di chuyển camera, giúp tạo ra một trải nghiệm mượt mà hơn khi tương tác với các phần tử 3D.
 controls.enableDamping = true
-controls.addEventListener('change', function () {
-    xLine.position.copy(controls.target)
-    yLine.position.copy(controls.target)
-    zLine.position.copy(controls.target)
 
-    raycaster.set(controls.target, dir.subVectors(camera.position, controls.target).normalize())
+const pickableObjects: THREE.Mesh[] = []
+let intersectedObject: THREE.Object3D | null
+const originalMaterials: { [id: string]: THREE.Material | THREE.Material[] } = {}
+const highlightedMaterial = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    color: 0x00ff00,
+})
 
-    intersects = raycaster.intersectObjects(sceneMeshes, false)
-    if (intersects.length > 0) {
-        if (intersects[0].distance < controls.target.distanceTo(camera.position)) {
-            camera.position.copy(intersects[0].point)
-        }
+const loader = new GLTFLoader()
+loader.load(
+    'models/simplescene.glb',
+    function (gltf) {
+        gltf.scene.traverse(function (child) {
+            if ((child as THREE.Mesh).isMesh) {
+                const m = child as THREE.Mesh
+                //the sphere and plane will not be mouse picked. THe plane will receive shadows while everything else casts shadows.
+                switch (m.name) {
+                    case 'Plane':
+                        m.receiveShadow = true
+                        break
+                    case 'Sphere':
+                        m.castShadow = true 
+                        break
+                    default:
+                        m.castShadow = true
+                        pickableObjects.push(m)
+                        //store reference to original materials for later
+                        originalMaterials[m.name] = (m as THREE.Mesh).material
+                }
+            }
+        })
+        scene.add(gltf.scene)
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
     }
-})
-
-const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
 )
-floor.rotateX(-Math.PI / 2)
-floor.position.y = -1
-scene.add(floor)
-sceneMeshes.push(floor)
-
-const wall1 = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 2),
-    new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
-)
-wall1.position.x = 4
-wall1.rotateY(-Math.PI / 2)
-scene.add(wall1)
-sceneMeshes.push(wall1)
-
-const wall2 = new THREE.Mesh(
-    new THREE.PlaneGeometry(2, 2),
-    new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
-)
-wall2.position.z = -3
-scene.add(wall2)
-sceneMeshes.push(wall2)
-
-const cube: THREE.Mesh = new THREE.Mesh(new THREE.BoxGeometry(), new THREE.MeshNormalMaterial())
-cube.position.set(-3, 0, 0)
-scene.add(cube)
-sceneMeshes.push(cube)
-
-const ceiling = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
-)
-ceiling.rotateX(Math.PI / 2)
-ceiling.position.y = 3
-scene.add(ceiling)
-sceneMeshes.push(ceiling)
-
-//crosshair
-const lineMaterial = new THREE.LineBasicMaterial({
-    color: 0x0000ff,
-})
-const points: THREE.Vector3[] = []
-points[0] = new THREE.Vector3(-0.1, 0, 0)
-points[1] = new THREE.Vector3(0.1, 0, 0)
-let lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const xLine = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(xLine)
-points[0] = new THREE.Vector3(0, -0.1, 0)
-points[1] = new THREE.Vector3(0, 0.1, 0)
-lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const yLine = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(yLine)
-points[0] = new THREE.Vector3(0, 0, -0.1)
-points[1] = new THREE.Vector3(0, 0, 0.1)
-lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-const zLine = new THREE.Line(lineGeometry, lineMaterial)
-scene.add(zLine)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -190,6 +146,34 @@ function onWindowResize() {
      * và độ phân giải trên màn hình khi kích thước của cửa sổ trình duyệt thay đổi.
      */
 }
+
+const raycaster = new THREE.Raycaster()
+let intersects: THREE.Intersection[]
+
+const mouse = new THREE.Vector2()
+
+function onDocumentMouseMove(event: MouseEvent) {
+    mouse.set(
+        (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+        -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+    )
+    raycaster.setFromCamera(mouse, camera)
+    intersects = raycaster.intersectObjects(pickableObjects, false)
+
+    if (intersects.length > 0) {
+        intersectedObject = intersects[0].object
+    } else {
+        intersectedObject = null
+    }
+    pickableObjects.forEach((o: THREE.Mesh, i) => {
+        if (intersectedObject && intersectedObject.name === o.name) {
+            pickableObjects[i].material = highlightedMaterial
+        } else {
+            pickableObjects[i].material = originalMaterials[o.name]
+        }
+    })
+}
+document.addEventListener('mousemove', onDocumentMouseMove, false)
 
 // ===================================================================================
 
